c9e68d7c2cd4f2bc84c0b0dc30c57df87dd57df6
Gabriel Jardim <gabrielrlj99@outlook.com>
2021-02-08 15:24:31 -0300

    currency-exchange-service

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+insert into currency_exchange(
+	currency_from, currency_to, conversion_multiple, environment
+)
+values(
+	'USD', 'BRL', 6, ''
+);
+
+insert into currency_exchange(
+	currency_from, currency_to, conversion_multiple, environment
+)
+values(
+	'EUR', 'BRL', 7, ''
+);
+
+insert into currency_exchange(
+	currency_from, currency_to, conversion_multiple, environment
+)
+values(
+	'AUD', 'BRL', 4, ''
+);
---
+++ src/test/java/com/jardim/currencyexchangeservices/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jardim.currencyexchangeservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
